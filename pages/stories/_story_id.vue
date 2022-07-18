0222de35520df3dec93ba9cf32d30f13aac980b5
Favour Felix <favour@acumen.digital>
2022-07-18 23:31:10 +0100

    integrated the storiy page

--- pages/stories/_story_id.vue
+++ pages/stories/_story_id.vue
@@ -2,11 +2,15 @@
   <div class="story-ctn section">
     <div class="inner">
       <div class="header">
-        {{ story.title[0] }}
+        <h2>
+          {{ story.title[0] }}
+        </h2>
+        <div class="pub-date">published on <b>{{ story.pubDate[0].slice(0, story.pubDate[0].length - 7) }}</b></div>
+        <div class="tags">
+          <nuxt-link v-for="(tag, index) in story.category" :key="tag" to="#" class="primary-btn">{{ index }} - {{ tag }}</nuxt-link>
+        </div>
       </div>
-      <div class="hero-img">
-        <img src="~assets/bitmaps/spotify.png" alt="">
-      </div>
+      <div class="hero-img" :style="`background-image: url(${story.image});`" />
       <p v-html="story['content:encoded'][0]" class="content" />
     </div>
   </div>
@@ -27,22 +31,61 @@ export default {
 </script>
 
 <style scoped>
+.tags {
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  overflow-x: auto;
+}
+.primary-btn {
+  margin-top: 6px;
+  display: flex;
+  height: 35px;
+  padding: 0 16px;
+  justify-content: space-between;
+  align-items: center;
+  outline: none;
+  border: 2px solid #000000;
+  font-size: 14px;
+  background: #DE8500;
+  color: #000000;
+  border-right: 0;
+  transition: 0.2s;
+  text-decoration: none;
+}
+.primary-btn:hover {
+  background: #9E5F00;
+  color: #FFDEAC;
+}
+.primary-btn:nth-last-child(1) {
+  border-right: 2px solid #000000;
+}
 .section > .inner {
   padding: 50px 0;
   padding-bottom: 15%;
   width: 70%;
   margin: 0 auto;
 }
-.hero-img img {
+.hero-img {
   width: 100%;
+  height: 500px;
+  background-repeat: no-repeat;
+  background-size: cover;
   margin: 5% 0;
 }
-.header {
+.header > h2 {
   font-style: normal;
   font-weight: 500;
-  font-size: 36px;
-  line-height: 40px;
+  font-size: 2.3rem;
+  line-height: 3.3rem;
   color: #000000;
+  margin-bottom: 8px;
+}
+.pub-date {
+  margin-bottom: 1.5rem;
+}
+.pub-date b {
+  font-weight: 500;
 }
 .content {
   font-weight: normal;
--- store/index.js
+++ store/index.js
@@ -7,7 +7,9 @@ export const state = () => ({
 export const mutations = {
   setStories (state, value) {
     const formattedStories = value
+    const imageLinkRegex = /<img\s+(?:[^>]*?\s+)?src=(["'])(.*?)\1/m
     for (const story of formattedStories) {
+      story.image = story['content:encoded'][0].match(imageLinkRegex)[2]
       story.id = story.title[0].replaceAll(': ', '-')
         .replaceAll(' ', '-')
         .replaceAll('\'', '')
