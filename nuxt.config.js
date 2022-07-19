d03028b95898d15d5d7f8cd042c53f4b7379c226
Favour Felix <favour@acumen.digital>
2022-07-19 01:59:28 +0100

    made target static

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,8 @@ export default {
     htmlAttrs: {
       lang: 'en'
     },
+    target: 'static',
+    ssr: false,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
