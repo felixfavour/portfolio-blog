6ad283972b8ac6e46b5eb6a57c14eb1adb75260d
Favour Felix <favour@acumen.digital>
2022-08-20 15:22:00 +0100

    chore: removed node version specification and turned ssr on

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,8 +5,7 @@ export default {
     htmlAttrs: {
       lang: 'en'
     },
-    target: 'static',
-    ssr: false,
+    ssr: true,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
