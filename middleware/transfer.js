a24ff216ce137ec548b24cf5ae51cbfc88318be8
felixfavour <felixfavour0@gmail.com>
2022-12-08 18:11:04 +0100

    chore: added redirect middleware

---
+++ middleware/transfer.js
@@ -0,0 +1,3 @@
+export default function ({ redirect }) {
+  return redirect('https://favourfelix.com/stories')
+}
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,10 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'static',
+  ssr: true,
+  router: {
+    middleware: 'transfer'
+  },
   head: {
     title: "Favour Felix's Stories - Stories about everything",
     htmlAttrs: {
